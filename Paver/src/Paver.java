df1c372946ea5293fc1998e2af511d232223948e
Matthew <horton.matthew.john@gmail.com>
2020-05-11 01:43:41 -0500

    remove test class

--- Paver/src/Paver.java
+++ Paver/src/Paver.java
@@ -1,9 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Paver {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner SC = new Scanner(new File("paver.dat"));
+		int cases = SC.nextInt();
+		SC.nextLine();
+		for (int i = 0; i < cases; i++) {
+			int price = SC.nextInt();
+			SC.nextLine();
+		}		
 	}
-
+	
+	public static int[][] buildBlankMatrix(int size){
+		if(size == 0) return null;
+		int[][] matrix = new int[size][size];
+		for (int i = 0; i < matrix.length; i++) 
+			for (int j = 0; j < matrix[i].length; j++) 
+				matrix[i][j] = -1;
+		return matrix;
+	}
 }
